d8a7c8cef9381a004e88645d9e6a0747b255b602
Kaze <75418111+KazeKaze93@users.noreply.github.com>
2026-07-18 09:32:56 +0400

    Wire Style Grid API routes into PromptServer on import.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
+from server import PromptServer
+
 from .nodes.style_grid_node import StyleGridNode
+from .stylegrid.routes import register_api
 
 NODE_CLASS_MAPPINGS = {
     "StyleGridNode": StyleGridNode,
@@ -9,5 +12,7 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 }
 
 WEB_DIRECTORY = "./web"
+
+register_api(PromptServer.instance.routes)
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
